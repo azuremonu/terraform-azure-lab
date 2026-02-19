faea1c85f8887d7a23e0c369ecae0d9bd05c19d9
Monu Kumar <azure.monu@gmail.com>
2026-02-19 17:15:19 +0530

    Creating VM with public ip

--- main.tf
+++ main.tf
@@ -33,6 +33,29 @@ resource "azurerm_network_interface" "nicdetails" {
     public_ip_address_id = azurerm_public_ip.pubipdetails.id
   }
 }
+resource "azurerm_public_ip" "pubipdetails" {
+  name                = "terra_pub_ip"
+  resource_group_name = azurerm_resource_group.rgdetails.name
+  location            = azurerm_resource_group.rgdetails.location
+  allocation_method   = "Static"
+}
+resource "azurerm_network_security_group" "nsgdetails" {
+  name                = "terra-nsg"
+  location            = azurerm_resource_group.rgdetails.location
+  resource_group_name = azurerm_resource_group.rgdetails.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 # Virtual Machine
 resource "azurerm_linux_virtual_machine" "vmdetails" {
   name                            = "terra-vm"
