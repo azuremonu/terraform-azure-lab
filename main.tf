18e9455d9c3957145fc1c2ae708da168f90e16e1
Monu Kumar <azure.monu@gmail.com>
2026-02-19 18:07:43 +0530

    Attach NSG to NIC and allow SSH

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "azurerm_resource_group" "rgdetails" {
   name     = "rg-monu"
   location = "West US 2"    # ← Ye karo
 }
+#VNET
 resource "azurerm_virtual_network" "vnetdetails" {
   name                = "terra-network"
   location            = azurerm_resource_group.rgdetails.location
@@ -9,18 +10,21 @@ resource "azurerm_virtual_network" "vnetdetails" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
+#Subnet 1
 resource "azurerm_subnet" "subnet1details" {
   name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rgdetails.name
   virtual_network_name = azurerm_virtual_network.vnetdetails.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+#Subnet 2
 resource "azurerm_subnet" "subnet2details" {
   name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.rgdetails.name
   virtual_network_name = azurerm_virtual_network.vnetdetails.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+# Network Interface
 resource "azurerm_network_interface" "nicdetails" {
   name                = "terra-nic"
   location            = azurerm_resource_group.rgdetails.location
@@ -33,12 +37,14 @@ resource "azurerm_network_interface" "nicdetails" {
     public_ip_address_id = azurerm_public_ip.pubipdetails.id
   }
 }
+#Public IP
 resource "azurerm_public_ip" "pubipdetails" {
   name                = "terra_pub_ip"
   resource_group_name = azurerm_resource_group.rgdetails.name
   location            = azurerm_resource_group.rgdetails.location
   allocation_method   = "Static"
 }
+#NSG
 resource "azurerm_network_security_group" "nsgdetails" {
   name                = "terra-nsg"
   location            = azurerm_resource_group.rgdetails.location
@@ -51,10 +57,15 @@ resource "azurerm_network_security_group" "nsgdetails" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "3389"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
+# NIC association with NSG
+resource "azurerm_network_interface_security_group_association" "nsglink" {
+  network_interface_id = azurerm_network_interface.nicdetails.id
+  network_security_group_id = azurerm_network_security_group.nsgdetails.id
 }
 # Virtual Machine
 resource "azurerm_linux_virtual_machine" "vmdetails" {
