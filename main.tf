511c7e5e3c57912906b5e060d4cd65e76a29c756
Monu Kumar <azure.monu@gmail.com>
2026-02-24 20:37:37 +0530

    Creating resources with data source error 7.0

--- main.tf
+++ main.tf
@@ -151,8 +151,8 @@ resource azurerm_linux_virtual_machine "vm_details" {
   }
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
