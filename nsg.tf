0bd542b1cf1c642d939a87de2af71f7c0ca0872f
Monu Kumar <azure.monu@gmail.com>
2026-02-25 20:17:26 +0530

    destroying resources

---
+++ nsg.tf
@@ -0,0 +1,21 @@
+/*
+resource "azurerm_network_security_group" "nsgdetails" {
+  name = "demo-nsg"
+  location = data.azurerm_resource_group.rgdetails.location
+  resource_group_name = data.azurerm_resource_group.rgdetails.name
+}
+resource "azurerm_network_security_rule" "demo-nsg-rules" {
+
+  for_each = var.security_rules
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "${each.value.destination_port_range}"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.rgdetails.name
+  network_security_group_name = azurerm_network_security_group.nsgdetails.name
+}
