014c13444694c1214837f414c304f393df5a1cbb
Monu Kumar <azure.monu@gmail.com>
2026-03-01 11:03:22 +0530

    refactor: remove old files and add module structure

---
+++ modules/resource_group/outputs.tf
@@ -0,0 +1,6 @@
+output "name" {
+  value = azurerm_resource_group.rgdetails
+}
+output "location" {
+  value = azurerm_resource_group.rgdetails.location
+}
--- rg.tf
+++
@@ -1,4 +0,0 @@
-
-data azurerm_resource_group "rgdetails" {
-  name = "data-rg"
-}
