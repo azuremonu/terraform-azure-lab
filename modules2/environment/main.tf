45d75326a9d7138cd390a753ffd7eceb21d8ef4c
Monu Kumar <azure.monu@gmail.com>
2026-03-02 11:39:56 +0530

    creating self module error 3

--- modules2/environment/main.tf
+++ modules2/environment/main.tf
@@ -3,13 +3,13 @@ resource "azurerm_resource_group" "dev-rg" {
   location = var.location
 }
 resource "azurerm_virtual_network" "dev-vnet" {
-  name = "dev-vnet"
+  name = "devv-vnet"
   location = var.location
   resource_group_name = azurerm_resource_group.dev-rg.name
   address_space = [var.vnet_address_space]
 }
 resource "azurerm_subnet" "dev-subnet" {
-  name = "dev-subnet"
+  name = "devv-subnet"
   resource_group_name = azurerm_resource_group.dev-rg.name
   virtual_network_name = azurerm_virtual_network.dev-vnet.name
   address_prefixes = [var.subnet_prefix]
