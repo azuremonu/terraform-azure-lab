fa6d35bee03bc46881799cf7a53fbbe553c9dfc9
Monu Kumar <azure.monu@gmail.com>
2026-03-05 11:30:37 +0530

    saving database modeules file

--- main.tf
+++ main.tf
@@ -22,3 +22,39 @@ module "stage" {
   subnet_prefix = "10.3.0.0/24"
   storage_account_name = "stagingstoragelab0018969"
 }
+
+module "dev_database" {
+  source              = "./modules/database"
+  env_name            = "dev"
+  location            = "eastus"
+  resource_group_name = module.dev.resource_group_name
+  vnet_name           = module.dev.vnet_name
+  db_subnet_prefix    = "10.1.1.0/24"
+  sql_admin_username  = "sqladmin"
+  sql_admin_password  = "P@ssword1234!"
+  backup_storage_name = "devdbbackup0018969"
+}
+
+module "prod_database" {
+  source              = "./modules/database"
+  env_name            = "prod"
+  location            = "eastus"
+  resource_group_name = module.prod.resource_group_name
+  vnet_name           = module.prod.vnet_name
+  db_subnet_prefix    = "10.2.1.0/24"
+  sql_admin_username  = "sqladmin"
+  sql_admin_password  = "P@ssword1234!"
+  backup_storage_name = "proddbbackup0018969"
+}
+
+module "stage_database" {
+  source              = "./modules/database"
+  env_name            = "stage"
+  location            = "eastus"
+  resource_group_name = module.stage.resource_group_name
+  vnet_name           = module.stage.vnet_name
+  db_subnet_prefix    = "10.3.1.0/24"
+  sql_admin_username  = "sqladmin"
+  sql_admin_password  = "P@ssword1234!"
+  backup_storage_name = "stagedbbackup0018969"
+}
