98c85ad577cbca59d77e451d864390b1f624c01a
Monu Kumar <azure.monu@gmail.com>
2026-03-08 11:59:11 +0530

    changing RG resions to all RG by variable

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_resource_group" "rgdetails" {
   name     = "demo-rg-01"
-  location = "eastus"
+  location = var.location
 }
 resource "azurerm_resource_group" "rgdetails2" {
   name     = "demo-rg-02"
-  location = "westus"
+  location = var.location
 }
 resource "azurerm_resource_group" "rgdetails3" {
   name     = "demo-rg-03"
-  location = "centralus"
+  location = var.location
 }
 resource "azurerm_resource_group" "rgdetails4" {
   name     = "demo-rg-04"
-  location = "eastus2"
+  location = var.location
 }
 resource "azurerm_resource_group" "rgdetails5" {
   name     = "demo-rg-05"
-  location = "westus2" 
+  location = var.location
 }
