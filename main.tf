59024ce60d59e654570e180fe46820a705ea600c
Monu Kumar <azure.monu@gmail.com>
2026-03-08 12:17:35 +0530

    creating RG by count

--- main.tf
+++ main.tf
@@ -1,20 +1,5 @@
 resource "azurerm_resource_group" "rgdetails" {
-  name     = "demo-rg-01"
-  location = var.location
-}
-resource "azurerm_resource_group" "rgdetails2" {
-  name     = "demo-rg-02"
-  location = var.location
-}
-resource "azurerm_resource_group" "rgdetails3" {
-  name     = "demo-rg-03"
-  location = var.location
-}
-resource "azurerm_resource_group" "rgdetails4" {
-  name     = "demo-rg-04"
-  location = var.location
-}
-resource "azurerm_resource_group" "rgdetails5" {
-  name     = "demo-rg-05"
+  count    = 5
+  name     = "demo-rg-${count.index}"
   location = var.location
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 variable "location" {
   default = "japaneast"
+  type = string
 }
