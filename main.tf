7a180bd6b3a8c2c02007265e38e98eaca4526c77
Monu Kumar <azure.monu@gmail.com>
2026-03-08 12:39:03 +0530

    creating RG by foreach 1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rgdetails" {
   for_each = toset(["dev", "prod", "stage"])
-  name     = "demo-rg-${for_each.key}"
+  name     = "demo-rg-${each.value}"
   location = var.location
 }
