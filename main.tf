61963f8f2b626285aad41f3ebbafe9ae8ae3fb19
Monu Kumar <azure.monu@gmail.com>
2026-03-08 12:46:12 +0530

    craeting RG by foreach 2

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rgdetails" {
   for_each = toset(["dev", "prod", "stage"])
-  name     = "demo-rg-${each.value}"
+  name     = "${each.value}-rg"
   location = var.location
 }
