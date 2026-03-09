c09facb218dfa65bea8135fc2f52300509cc016c
Monu Kumar <azure.monu@gmail.com>
2026-03-09 11:12:30 +0530

    creating RG with help of Vraibles and modules1

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,7 +1,3 @@
-resource "azurerm_resource_group" "rgdetails" {
-  name = "dev-rg"
-  location = var.location
-}
 resource "azurerm_resource_group" "rgdetails2" {
   name = "prd-rg"
   location = var.location
