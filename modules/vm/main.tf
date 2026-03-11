dd7aabc2a2efbdb2e2d490fca80448dfed68079c
Monu Kumar <azure.monu@gmail.com>
2026-03-11 11:27:30 +0530

    Add all 5 Terraform modules error 5

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                            = var.vm_name
   location                        = var.location
   resource_group_name             = var.resource_group_name
-  size                            = "Standard_B2s"
+  size                            = "Standard_D2s_v3"
   admin_username                  = var.admin_username
   admin_password                  = var.admin_password
   disable_password_authentication = false
